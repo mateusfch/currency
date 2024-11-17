1858b6f125a66ac634ada593aae8f04d1643451a
mateusfch <charloto.mateus@gmail.com>
2024-11-17 13:57:08 -0300

    wipwipwip

--- currency-conversion-service/src/main/java/com/engsoft2/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/engsoft2/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.engsoft2.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,14 +19,18 @@ public class CurrencyConversionController {
         this.proxy = proxy;
     }
 
+    @Value("${currency-exchange-service.url}")
+    private String currencyExchangeBaseUrl;
+    
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
             @PathVariable BigDecimal quantity) {
         HashMap<String, String> uriVariables = new HashMap<>();
         uriVariables.put("from", from);
         uriVariables.put("to", to);
+        String url = String.format("%s/from/{from}/to/{to}", currencyExchangeBaseUrl);
         ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
-                "http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+                url, CurrencyConversion.class, uriVariables);
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
             CurrencyConversion currencyConversion = responseEntity.getBody();
             return new CurrencyConversion(
